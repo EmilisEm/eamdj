78eab53778d255562c2e849a00879b47b532bc2a
jurisbogd <juris.bogdanas@gmail.com>
2024-11-06 14:05:39 +0200

    Added default value to Product name to prevent possible null warning.

--- EAMDJ/Model/Product.cs
+++ EAMDJ/Model/Product.cs
@@ -3,7 +3,7 @@ namespace EAMDJ.Model;
 public record Product {
     public Guid Id { get; init; }
     public decimal Price { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     // Id of the product category to which this product belongs to.
     // Might be a good idea to add a string representation of this later, so a UI doesn't have to also fetch category
