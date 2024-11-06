b90d267a5e8eb4a03fea25af53f30c3099b690e1
jurisbogd <juris.bogdanas@gmail.com>
2024-11-06 13:59:57 +0200

    Appended comment with possible modifications to Product entity.

--- EAMDJ/Model/Product.cs
+++ EAMDJ/Model/Product.cs
@@ -9,4 +9,11 @@ public record Product {
     // Might be a good idea to add a string representation of this later, so a UI doesn't have to also fetch category
     // names when getting the orders.
     public Guid CategoryId { get; set; }
+
+    // Might be a good idea to merge modifiers and products. Could simplify price and tax calculations.
+    // In that case it should have a flag denoting whether or not it's a modifier, or something similar.
+    // public bool IsModifier { get; set; }
+    // Could either have nullable lists for possible modifiers/products that it can be a modifier for
+    // public List<Product>? CanModifyProducts { get; set; }
+    // public List<Product>? PossibleModifiers { get; set; }
 }
