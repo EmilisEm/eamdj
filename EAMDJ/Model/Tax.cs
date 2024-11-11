d158ac112af33ae5c285ae44f0bd7164508d2e00
jurisbogd <juris.bogdanas@gmail.com>
2024-11-11 17:10:18 +0200

    Added data annotations to Tax entity.

--- EAMDJ/Model/Tax.cs
+++ EAMDJ/Model/Tax.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EAMDJ.Model;
 
 public record Tax {
-    public Guid Id { get; init; }
+    [Key] public Guid Id { get; init; }
     public string? Name { get; set; }
     public decimal Percentage { get; set; }
 
     // Data model does not specify, but implies a ProductCategoryId field.
-    public Guid ProductCategoryId { get; set; }
+    [ForeignKey("ProductCategory")] public Guid ProductCategoryId { get; set; }
 }
