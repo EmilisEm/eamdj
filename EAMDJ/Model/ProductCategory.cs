5f1d6406ddd74bbffd1e8545b792e19e21fbf025
jurisbogd <juris.bogdanas@gmail.com>
2024-11-11 14:35:29 +0200

    Changed string.Empty in ProductCategory to a nullable string.

--- EAMDJ/Model/ProductCategory.cs
+++ EAMDJ/Model/ProductCategory.cs
@@ -2,9 +2,9 @@ namespace EAMDJ.Model;
 
 public record ProductCategory {
     public Guid Id { get; init; }
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
     public Guid BusinessId { get; set; }
-    
+
     // Could be a good idea to add a ParentCategory field or something similar later.
     // Would allow more granularity with product categories for the end user.
     // 'Citrus fruit' is a subset of 'Fruit' which is a subset of 'Whole foods' etc. 
