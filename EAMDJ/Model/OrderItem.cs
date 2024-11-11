96fd986261904317564ffec5be6bd4df05712fb8
jurisbogd <juris.bogdanas@gmail.com>
2024-11-11 15:20:22 +0200

    Added some extra comments to OrderItem.

--- EAMDJ/Model/OrderItem.cs
+++ EAMDJ/Model/OrderItem.cs
@@ -4,4 +4,13 @@ public class OrderItem {
     public Guid ProductId { get; init; }
     public Guid OrderId { get; init; }
     public uint Quantity { get; set; }
+
+    // Not sure how to implement discounts.
+    // An option would be to have each OrderItem point to a specific discount, then there would be a limit of one
+    // discount per order item.
+    // public Guid DiscountId { get; set; }
+
+    // Another option would be to have an entity like OrderDiscounts, which would be able to unify Discounts and
+    // DiscountCoupons.
+    // Either way, I'm leaving DiscountCoupons for later.
 }
