bb82da6292106e3e70b66f3495efb1b39da96187
jurisbogd <juris.bogdanas@gmail.com>
2024-11-13 14:59:57 +0200

    Changed nullable string to string.Empty in Tax entity.

--- EAMDJ/Model/Tax.cs
+++ EAMDJ/Model/Tax.cs
@@ -5,7 +5,7 @@ namespace EAMDJ.Model;
 
 public record Tax {
     public Guid Id { get; init; }
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public decimal Percentage { get; set; }
 
     // Data model does not specify, but implies a ProductCategoryId field.
