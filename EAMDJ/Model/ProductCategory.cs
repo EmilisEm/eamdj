2fe4af828b164f5b96a2620074c64147e289fd70
jurisbogd <juris.bogdanas@gmail.com>
2024-11-13 15:00:51 +0200

    Removed unnecessary data annotations in ProductCategory entity.

--- EAMDJ/Model/ProductCategory.cs
+++ EAMDJ/Model/ProductCategory.cs
@@ -4,9 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace EAMDJ.Model;
 
 public record ProductCategory {
-    [Key] public Guid Id { get; init; }
+    public Guid Id { get; init; }
     public string? Name { get; set; }
-    [ForeignKey("Business")] public Guid BusinessId { get; set; }
+    public Guid BusinessId { get; set; }
 
     // Could be a good idea to add a ParentCategory field or something similar later.
     // Would allow more granularity with product categories for the end user.
