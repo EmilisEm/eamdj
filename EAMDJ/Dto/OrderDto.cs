d4115da0442e21c879be79421ec38a05717a1c6c
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-12-10 21:24:29 +0200

    Fixed formatting

--- EAMDJ/Dto/OrderDto.cs
+++ EAMDJ/Dto/OrderDto.cs
@@ -6,7 +6,7 @@ namespace EAMDJ.Dto
 	{
 		public Guid Id { get; init; }
 		public Guid BusinessId { get; init; }
-		public OrderStatus Status;
+		public OrderStatus Status { get; init; }
 		public DateTime CreatedAt { get; init; }
 		public DateTime LastModifiedAt { get; set; }
 	}
--- EAMDJ/Repository/CategoryRepository/ICategoryRepository.cs
+++ EAMDJ/Repository/CategoryRepository/ICategoryRepository.cs
@@ -3,13 +3,11 @@
 namespace EAMDJ.Repository.CategoryRepository
 {
 	public interface ICategoryRepository
-	{		
+	{
 		Task<ProductCategory> GetProductCategoryAsync(Guid id);
 		Task<IEnumerable<ProductCategory>> GetAllProductCategoriesByBusinessIdAsync(Guid businessId);
 		Task<ProductCategory> UpdateProductCategoryAsync(Guid id, ProductCategory productCategory);
 		Task DeleteProductCategoryAsync(Guid id);
 		Task<ProductCategory> CreateProductCategoryAsync(ProductCategory productCategory);
-
-
 	}
 }
